56221f30e1cb316150bc522ae0650bcd7f60167d
vickymarimuthu0512@outlook.com <vickymarimuthu0512@outlook.com>
2022-02-07 15:39:12 +0530

    [VIGNESH M] Added: 3 Tier Architecture and Custom Exception Class in Repository Layer

---
+++ CommonLayer/UserModel/SignUpModel.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="SignUpModel.cs" company="Book Store Application">
+//     SignUpModel copyright tag.
+// </copyright>
+
+/// <summary>
+/// Model for user signup
+/// </summary>
+namespace CommonLayer.UserModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// SignupModel Class Entities
+    /// </summary>
+    public class SignUpModel
+    {
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        /// The user identifier.
+        /// </value>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        /// <value>
+        /// The full name.
+        /// </value>
+        [Required(ErrorMessage = "FullName is required")]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email identifier.
+        /// </summary>
+        /// <value>
+        /// The email identifier.
+        /// </value>
+        [Required(ErrorMessage = "EmailId is required")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string EmailId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        [Required(ErrorMessage = "Password Is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mobile number.
+        /// </summary>
+        /// <value>
+        /// The mobile number.
+        /// </value>
+        [Required(ErrorMessage = "Mobile Number Is required")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Password Name")]
+        public string MobileNumber { get; set; }
+    }
+}
